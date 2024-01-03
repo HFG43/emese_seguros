b08ccb156bfb64b101271ae309a7f9cd42d37f82
HFG43 <hernanguemes@hotmail.com>
2024-01-03 17:18:41 -0300

    Feat: User and Risk Migration definition

--- db/migrate/20231118235130_create_users.rb
+++ db/migrate/20231118235130_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
+      t.string :name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.integer :document, null: false
+      t.string :phone_number, null: false
+      
       t.timestamps
     end
   end
--- db/migrate/20231122195937_create_risks.rb
+++ db/migrate/20231122195937_create_risks.rb
@@ -1,6 +1,7 @@
 class CreateRisks < ActiveRecord::Migration[7.0]
   def change
     create_table :risks do |t|
+      t.string :type, null: false
 
       t.timestamps
     end
