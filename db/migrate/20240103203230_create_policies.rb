6c32cd645e48062170cbe445beba8add222e87b9
HFG43 <hernanguemes@hotmail.com>
2024-01-04 12:11:47 -0300

    Feat: add policy migration

--- db/migrate/20240103203230_create_policies.rb
+++ db/migrate/20240103203230_create_policies.rb
@@ -1,7 +1,12 @@
 class CreatePolicies < ActiveRecord::Migration[7.0]
   def change
     create_table :policies do |t|
-
+      t.references :user, null: false, foreign_key: { to_table: :users }
+      t.references :risk, null: false, foreign_key: { to_table: :risks}
+      t.string :numer, null: false
+      t.string :company, null: false
+      t.string :description, null: false
+      
       t.timestamps
     end
   end
