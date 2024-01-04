f8dea4e1f871f9d7daa6010e1b5701b36e0575ce
HFG43 <hernanguemes@hotmail.com>
2024-01-04 12:45:02 -0300

    Misc: correct number typo

--- db/migrate/20240103203230_create_policies.rb
+++ db/migrate/20240103203230_create_policies.rb
@@ -3,10 +3,10 @@ class CreatePolicies < ActiveRecord::Migration[7.0]
     create_table :policies do |t|
       t.references :user, null: false, foreign_key: { to_table: :users }
       t.references :risk, null: false, foreign_key: { to_table: :risks}
-      t.string :numer, null: false
+      t.string :number, null: false
       t.string :company, null: false
       t.string :description, null: false
-      
+
       t.timestamps
     end
   end
