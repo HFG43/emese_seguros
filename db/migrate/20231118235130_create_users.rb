cf4fbd493c4be600c2014bae74d71c637ea63ceb
HFG43 <hernanguemes@hotmail.com>
2024-01-04 13:08:11 -0300

    Build: configure devise gem

--- db/migrate/20231118235130_create_users.rb
+++ db/migrate/20231118235130_create_users.rb
@@ -3,10 +3,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name, null: false
       t.string :last_name, null: false
-      t.string :email, null: false
       t.integer :document, null: false
       t.string :phone_number, null: false
-      
+
       t.timestamps
     end
   end
