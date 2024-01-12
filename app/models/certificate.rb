87e887faf421f2de58e5772367821cfdfe384fe0
HFG43 <hernanguemes@hotmail.com>
2024-01-12 11:12:54 -0300

    Feat: add User/Policy/Certificate associations throught Models

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,3 +1,8 @@
 class Certificate < ApplicationRecord
   has_one_attached :file
+  belongs_to :policy, class_name: 'Policy', foreign_key: 'policy_id'
+
+  validates :type, presence: true
+  validates :year, presence: true
+  validates :month, presence: true
 end
--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,2 +1,8 @@
 class Policy < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :certificates, dependent: :destroy
+
+  validates :number, presence: true
+  validates :company, presence: true
+  validates :description, presence: true
 end
